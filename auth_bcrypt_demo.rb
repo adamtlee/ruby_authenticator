dc3b250ed3e7ab793e8a84d35671ce9ebf625c9d
adam <adam.lee346@gmail.com>
2021-07-12 09:52:25 -0600

    example hashes bcrypt

--- auth_bcrypt_demo.rb
+++ auth_bcrypt_demo.rb
@@ -13,13 +13,20 @@
 require 'bcrypt'
 
 my_password = BCrypt::Password.create("my password")
-puts my_password
+my_password_1 = BCrypt::Password.create("my password")
+my_password_2 = BCrypt::Password.create("my password")
+
+
+puts my_password 
+puts my_password_1
+puts my_password_2
+
 
 # encrypts to this value
-my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
+# my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
 
 # string comparison of the actual password
-puts my_password == "my password"     #=> true
+# puts my_password == "my password"     #=> true
 
 # my_password == "not my password" #=> false
 
